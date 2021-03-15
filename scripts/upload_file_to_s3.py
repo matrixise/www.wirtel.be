c72baba34754702caebbc70f48720a060cbf7701
Stéphane Wirtel <stephane.wirtel@mgx.io>
2021-03-15 17:14:56 +0100

    Build CV (#1)
    Daily Build
    Upload the PDF file to S3

---
+++ scripts/upload_file_to_s3.py
@@ -0,0 +1,22 @@
+import os
+import boto3
+from botocore.exceptions import ClientError
+from botocore.config import Config
+
+
+BUCKET_NAME = 'public-mgxio'
+FOLDER_NAME = 'wirtel.be'
+file_name = 'StephaneWirtel.pdf'
+
+config = Config(
+    region_name = 'eu-west-3',
+)
+
+s3_client = boto3.client('s3', config=config)
+
+response = s3_client.upload_file(
+    os.path.join(os.getcwd(), file_name),
+    BUCKET_NAME,
+    f'{FOLDER_NAME}/{file_name}'
+)
+
