54dfb36c15199d7190159d030c49610b5b1cf893
Stéphane Wirtel <stephane@wirtel.be>
2021-03-16 14:36:13 +0100

    ACL = public-read for the CV

--- scripts/upload_file_to_s3.py
+++ scripts/upload_file_to_s3.py
@@ -17,6 +17,7 @@ s3_client = boto3.client('s3', config=config)
 response = s3_client.upload_file(
     os.path.join(os.getcwd(), file_name),
     BUCKET_NAME,
-    f'{FOLDER_NAME}/{file_name}'
+    f'{FOLDER_NAME}/{file_name}',
+    ExtraArgs = {'ACL': 'public-read'}
 )
 
